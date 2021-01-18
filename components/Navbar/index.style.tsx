73425a6cb9c5fabffc458fdaddd532911a6aa496
ahd <ahdhani0@gmail.com>
2021-01-18 11:16:28 +0300

    convert the navbar to a phone styled navigation

--- components/Navbar/index.style.tsx
+++ components/Navbar/index.style.tsx
@@ -1,6 +1,4 @@
-import styled, { css } from "styled-components";
-import Link from "next/link";
-import { Page } from "../../types";
+import styled from "styled-components";
 
 export const Wrapper = styled.nav<{ scrolled: boolean }>`
   position: fixed;
@@ -10,87 +8,36 @@ export const Wrapper = styled.nav<{ scrolled: boolean }>`
   top: 0;
   left: 0;
   padding: 1rem;
-  flex-direction: column;
-  align-items: flex-end;
-  justify-content: center;
-  background: ${({ theme: { colors }, scrolled }) =>
-    scrolled ? colors.white : "transparent"};
-  transition: 0.3s ease-out;
-  box-shadow: ${({ scrolled }) =>
-    scrolled ? "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;" : "none"};
-`;
-
-export const MenuButton = styled.button<{ toggle: boolean; scrolled: boolean }>`
-  background: transparent;
-  border: none;
-  padding: 0;
-  margin: 0;
-  cursor: pointer;
-  outline: none;
-  > svg {
-    height: 1.3rem;
-    fill: ${({ theme: { colors }, scrolled }) =>
-      scrolled ? colors.primary : colors.white};
-    > path {
-      transition: transform 0.3s ease-in-out;
-    }
-  }
-  ${({ toggle }) =>
-    toggle &&
-    css`
-      > svg {
-        path:nth-child(1) {
-          transform: translateX(-40%);
-        }
-        path:nth-child(3) {
-          transform: translateX(40%);
-        }
-      }
-    `}
-`;
-
-const MenuContainer = styled.ul<{ toggle: boolean; pagesLength: number }>`
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-  display: flex;
+  align-items: center;
   justify-content: space-between;
-  width: 100%;
-  margin-top: 1rem;
-  display: ${({ toggle }) => (toggle ? "flex" : "none")};
-  overflow: hidden;
-  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.md}) {
-    justify-content: space-evenly;
+  box-sizing: border-box;
+  background: ${({ scrolled }) => (scrolled ? "#fff" : "transparent")};
+  transition: all 0.4s ease-in-out;
+  box-shadow: ${({ scrolled }) =>
+    scrolled
+      ? "0px 3px 4px rgba(0, 0, 0, 0.1)"
+      : "0px 3px 4px rgba(0, 0, 0, 0)"};
+  > button,
+  a {
+    color: ${({ theme: { colors }, scrolled }) =>
+      scrolled ? "#333333" : colors.white};
   }
 `;
-const MenuItem = styled.li``;
-const MenuLink = styled.a<{ scrolled: boolean }>`
-  color: ${({ theme: { colors }, scrolled }) =>
-    scrolled ? colors.primary : colors.white};
-  font-weight: 500;
-  text-shadow: 0px 0px 2px rgba(0, 0, 0, 0.1);
+
+export const BackButton = styled.button`
+  font-family: inherit;
+  background: transparent;
+  font-weight: 800;
+  border: none;
+  text-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+  font-size: 1.3rem;
 `;
 
-export const Menu = ({
-  pages,
-  toggle,
-  scrolled,
-}: {
-  toggle: boolean;
-  pages: Page[];
-  scrolled: boolean;
-}) => {
-  return (
-    <MenuContainer toggle={toggle} pagesLength={pages.length}>
-      {pages.map((item: Page) => {
-        return (
-          <MenuItem key={item.title}>
-            <Link href={item.href} passHref>
-              <MenuLink scrolled={scrolled}>{item.title}</MenuLink>
-            </Link>
-          </MenuItem>
-        );
-      })}
-    </MenuContainer>
-  );
-};
+export const HomeLink = styled.a`
+  font-family: inherit;
+  background: transparent;
+  font-weight: 800;
+  border: none;
+  text-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+  font-size: 1.3rem;
+`;
